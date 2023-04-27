7ed42734f9bbca0296e174f7ae9cb3f7600d3589
Jaeyong Choi <lkmdl2021@gmail.com>
2023-04-27 12:45:49 +0900

    5th

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ window.addEventListener("DOMContentLoaded",()=>{
   console.log("2023.04.25 header layout complete");//3rd
   console.log("2023.04.25 layout ~");
   console.log("2023.04.26 layout ~");//4th
+  console.log("2023.04.27 layout ~");//5th
 
 
   
