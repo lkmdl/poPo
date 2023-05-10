f240d98ff8f4bd7b79b868da7fd8759a85b3dfff
Jaeyong Choi <lkmdl2021@gmail.com>
2023-05-10 16:05:03 +0900

    11th

--- js/index.js
+++ js/index.js
@@ -2,26 +2,12 @@ window.addEventListener("DOMContentLoaded",()=>{
   let empty1 = document.querySelector(".nav-menu");
   let empty2 = document.querySelector(".footer-nav-menu");
   
-  empty1.onclick = ()=> {
-    alert("준비중입니다...");
-
-  }
-  
-  empty2.onclick = ()=> {
+  empty1, empty2.onclick = ()=> {
     alert("준비중입니다...");
 
   }
 
-  console.log("2023.04.23 create");//1st
-  console.log("2023.04.24 start!!!!!!!");//2nd
-  console.log("2023.04.24 header ~ ");
-  console.log("2023.04.25 header layout complete");//3rd
-  console.log("2023.04.25 inner layout ~");
-  console.log("2023.04.26 inner layout ~ ");//4th
-  console.log("2023.04.27 inner layout ~");//5th
-  console.log("2023.05.02 layout remove");//6th
-  console.log("2023.05.03 contents nameing");//7th
-  console.log("2023.05.04 section2 add");//8th
+
 
 
 
