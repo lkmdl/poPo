9d1da14675ed10ac1ac9e271cbc1f1728625cd96
Jaeyong Choi <lkmdl2021@gmail.com>
2023-05-11 11:44:09 +0900

    12th

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded",()=>{
   let empty1 = document.querySelector(".nav-menu a");
   
   empty1.onclick = ()=> {
+    console.log("gd")
      alert("준비중입니다...");
 
   }
