e2d68428fbebc9123ea9098b9f800019a1e922e0
Jaeyong Choi <lkmdl2021@gmail.com>
2023-06-07 17:05:59 +0900

    22th

--- js/index.js
+++ js/index.js
@@ -12,12 +12,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
   }
 
-  const menu = document.querySelector('.navbar__menu');
+  const section = document.querySelector('section');
+  const toggleBtn = document.querySelector('.toggleBtn');
   const icons = document.querySelector('.navbar__icons');
-  const toggleBtn = document.querySelector('.navbar__toggleBtn');
 
   toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
+    section.classList.toggle('active');
     icons.classList.toggle('active');
     //menu의 클래스리스트에 'active' 클래스가 있다면 없애주고, 없다면 'active' 클래스를 추가는 역활을 한다.
   });
