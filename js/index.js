da2dd682f4a605cdc8625d345c601824a6c1ff32
Jaeyong Choi <lkmdl2021@gmail.com>
2023-06-13 20:25:22 +0900

    25th

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-  
+
   const empty = document.querySelectorAll(".nav-menu li a");
   for (let i = 0; i < empty.length; i++) {
     function em() {
@@ -13,13 +13,26 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const empty_box = document.querySelector('.empty_box');
   const toBtn = document.querySelector('.toBtn');
-  toBtn.addEventListener('click', function() {
+  toBtn.addEventListener('click', function () {
     toBtn.classList.toggle('on');
     empty_box.classList.toggle('on');
 
   });
 
-  
+  const aloneBtn = document.querySelector(".head-alone");
+  const teamBtn = document.querySelector(".nead-team");
+  const aloneCont = document.querySelector(".cont-alone");
+  const teamCont = document.querySelector(".cont-team");
+  aloneBtn.addEventListener('click', function () {
+    console.log("a");
+  });
+  teamBtn
+  aloneCont
+  teamCont
+
+
+
+
 
 
 
