b624259b4652eded0b28bdf16c3f84b86042173e
Jaeyong Choi <lkmdl2021@gmail.com>
2023-06-16 05:45:38 +0900

    26th

--- js/index.js
+++ js/index.js
@@ -8,14 +8,22 @@ window.addEventListener("DOMContentLoaded", () => {
     empty[i].addEventListener('click', em);
   }
 
+  // const toBtn = document.querySelector('.toBtn');
+  // const empty_box = toBtn.parentNode.nextElementSibling;
+  // toBtn.addEventListener('click', function () {
+  //   this.classList.toggle('on');
+  //   console.log(this)
+  //   empty_box.classList.toggle('on');
+  //   console.log(empty_box)
+
+  // });
+
   const s3_toBtn = document.querySelector('#section-3 .toBtn');
   const s3_empty_box = document.querySelector('#section-3 .empty_box');
   s3_toBtn.addEventListener('click', function () {
     s3_toBtn.classList.toggle('on');
     s3_empty_box.classList.toggle('on');
   });
-
-
 
   const s4_toBtn = document.querySelector('#section-4 .toBtn');
   const s4_empty_box = document.querySelector('#section-4 .empty_box');
