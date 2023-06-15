cfbdd7319d77b2350db139bb1cce6ff26f3af1d6
Jaeyong Choi <lkmdl2021@gmail.com>
2023-06-15 14:45:17 +0900

    26th

--- js/index.js
+++ js/index.js
@@ -18,10 +18,10 @@ window.addEventListener("DOMContentLoaded", () => {
     toBtn.classList.toggle('on');
     empty_box.classList.toggle('on');
   });
-
+  
   $(".head_alone span").on("click", function () {
-    $("#section-2 img:nth-child(1)").css("display", "block");
-    $("#section-2 img:nth-child(2)").css("display", "none");
+    $("#section-2 > div > img:nth-child(1)").css("display", "block");
+    $("#section-2 > div > img:nth-child(2)").css("display", "none");
     $(".head_alone span").css("text-decoration-line", "underline");
     $(".head_team span").css("text-decoration-line", "none");
     $(".cont-alone").css("display", "block");
@@ -30,12 +30,12 @@ window.addEventListener("DOMContentLoaded", () => {
   })
 
   $(".head_team span").on("click", function () {
-    $("#section-2 img:nth-child(1)").css("display", "none");
-    $("#section-2 img:nth-child(2)").css("display", "block");
-    $(".head_team span").css("text-decoration-line", "underline")
+    $("#section-2 > div > img:nth-child(1)").css("display", "none");
+    $("#section-2 > div > img:nth-child(2)").css("display", "block");
     $(".head_alone span").css("text-decoration-line", "none")
-    $(".cont-team").css("display", "block");
+    $(".head_team span").css("text-decoration-line", "underline")
     $(".cont-alone").css("display", "none");
+    $(".cont-team").css("display", "block");
 
   })
 
