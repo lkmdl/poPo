d2646fd49ef06ee84338d0b238198acc3d6b4e19
Jaeyong Choi <lkmdl2021@gmail.com>
2023-06-16 04:36:28 +0900

    26th

--- js/index.js
+++ js/index.js
@@ -1,43 +1,30 @@
 window.addEventListener("DOMContentLoaded", () => {
-
   const empty = document.querySelectorAll(".nav-menu li a");
   for (let i = 0; i < empty.length; i++) {
     function em() {
       alert("준비중입니다...");
       console.log("gd");
-
     }
     empty[i].addEventListener('click', em);
-
   }
 
-  const toBtn = document.querySelector('.toBtn');
-  const empty_box = document.querySelector('.empty_box');
-
-  toBtn.addEventListener('click', function () {
-    toBtn.classList.toggle('on');
-    empty_box.classList.toggle('on');
+  const s3_toBtn = document.querySelector('#section-3 .toBtn');
+  const s3_empty_box = document.querySelector('#section-3 .empty_box');
+  s3_toBtn.addEventListener('click', function () {
+    s3_toBtn.classList.toggle('on');
+    s3_empty_box.classList.toggle('on');
   });
-  
-  $(".head_alone span").on("click", function () {
-    $("#section-2 > div > img:nth-child(1)").css("display", "block");
-    $("#section-2 > div > img:nth-child(2)").css("display", "none");
-    $(".head_alone span").css("text-decoration-line", "underline");
-    $(".head_team span").css("text-decoration-line", "none");
-    $(".cont-alone").css("display", "block");
-    $(".cont-team").css("display", "none");
 
-  })
 
-  $(".head_team span").on("click", function () {
-    $("#section-2 > div > img:nth-child(1)").css("display", "none");
-    $("#section-2 > div > img:nth-child(2)").css("display", "block");
-    $(".head_alone span").css("text-decoration-line", "none")
-    $(".head_team span").css("text-decoration-line", "underline")
-    $(".cont-alone").css("display", "none");
-    $(".cont-team").css("display", "block");
 
-  })
+  const s4_toBtn = document.querySelector('#section-4 .toBtn');
+  const s4_empty_box = document.querySelector('#section-4 .empty_box');
+  s4_toBtn.addEventListener('click', function () {
+    s4_toBtn.classList.toggle('on');
+    s4_empty_box.classList.toggle('on');
+  });
+
+
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////
 });
