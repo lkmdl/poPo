61a81176eb7e1767ef1b02edf11bd4c3f0cd91c4
Jaeyong Choi <lkmdl2021@gmail.com>
2023-06-20 18:32:32 +0900

    27th

--- js/index.js
+++ js/index.js
@@ -8,31 +8,15 @@ window.addEventListener("DOMContentLoaded", () => {
     empty[i].addEventListener('click', em);
   }
 
-  // const toBtn = document.querySelector('.toBtn');
-  // const empty_box = toBtn.parentNode.nextElementSibling;
-  // toBtn.addEventListener('click', function () {
-  //   this.classList.toggle('on');
-  //   console.log(this)
-  //   empty_box.classList.toggle('on');
-  //   console.log(empty_box)
+  const toBtn = document.querySelectorAll('.toBtn');
+  for (let i = 0; i < toBtn.length; i++) {
+      toBtn[i].addEventListener("click", function () {
+        this.classList.toggle('on');
+        const Folding_box = this.parentNode.nextElementSibling;
+        Folding_box.classList.toggle('on');
 
-  // });
-
-  const s3_toBtn = document.querySelector('#section-3 .toBtn');
-  const s3_empty_box = document.querySelector('#section-3 .empty_box');
-  s3_toBtn.addEventListener('click', function () {
-    s3_toBtn.classList.toggle('on');
-    s3_empty_box.classList.toggle('on');
-  });
-
-  const s4_toBtn = document.querySelector('#section-4 .toBtn');
-  const s4_empty_box = document.querySelector('#section-4 .empty_box');
-  s4_toBtn.addEventListener('click', function () {
-    s4_toBtn.classList.toggle('on');
-    s4_empty_box.classList.toggle('on');
-  });
-
-
+      })    
+  }
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////
 });
