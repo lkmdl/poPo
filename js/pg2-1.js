dc38b35e2759f1de84c08f711d19540cdc442763
Jaeyong Choi <lkmdl2021@gmail.com>
2025-02-14 01:38:49 +0900

    long~time

--- js/pg2-1.js
+++ js/pg2-1.js
@@ -12,50 +12,71 @@ const dur_sc = 600;
 // 광스크롤 금지 시간, 스크롤애니시간
 // 5. 스크롤이징
 const easing_sc = "easeInOutQuint";
-
+///////////////////////////////////////////////////
 
 ////////////////// 로딩구역 ////////////////////
+
 $(function () { ////// jQB //////////////////////
+    // 3D 모델의 회전 상태를 다룰 변수
+    let rotationAngle = 0; // 회전 각도를 저장하는 변수
 
-    /************************************************* 
-        [ 자동스크롤 구현 ]
-        1. 기능: 마우스휠을 위나 아래로 작동시킬때
-        페이지가 위나 아래로 자동으로 애니메이션 되는 기능
-        2. 이벤트: 마우스휠을 움직일때 발생하는 이벤트는?
-        - mousewheel (오리지널 이벤트) -> 파이어폭스 지원안함!
-        - wheel (신규이벤트-벤더사의 웹표준이 아닌아님!)
-            -> 엣지, 사파리 지원안함!
-        - DOMMouseScroll (파이어폭스 전용이벤트)
-        -> 결론적으로 모두 한꺼번에 지원하는 이벤트가 없으므로
-        mousewheel + DOMMouseScroll 2개를 같이 사용할 것임!
-        3. 마우스 휠 이벤트와 함수를 연결하는 방법:
-        - on(이벤트명, 함수)
-        -> on 메서드로 어떤 이벤트와도 함수를 연결할 수 있다!
-        (참고로 bind(이벤트명,함수) 메서드를 사용했었지만
-        제이쿼리 버전3부터 지원중단함!)
-        4. 마우스 휠 이벤트 대상 : 보통 document에 적용함!
-        -> 중요한 업데이트 유의사항!!!
-        document, body, window 객체는 이벤트 막기를 할 수 없다!
-        - 2019년도에 위의 세가지 중요객체에 대하여 기본기능막기를
-        할 수 없도록 브라우저 소스가 업데이트 되었는데.... 이유는?
-        모바일에서 에러가 발생하는 문제의 원인 중 하나로 지목됨
-        본 3가지 중요객체에 대해서 e.preventDefault() 또는
-        return false 를 사용한 기능막기를 못하도록 하였다!
-        예) 안되는 케이스
-        $(document).on("click".function(e){
-            e.preventDefault(); -> 에러발생!
-        });
-        $(window).on("click".function(e){
-            return false; -> 에러발생!
-        });
-        $("body").on("click".function(e){
-            e.preventDefault(); -> 에러발생!
-        });
-        -> 에러를 우회하는 방법은 window, document, body대신
-        내부에 전체 부모박스를 하나 만들고 이것을 대신 사용함!
-        또 하나는 스크롤바를 안나오게 하여 코딩하는 방법이다!
-    *************************************************/
+    // 페이지에 맞는 회전각도 및 속도 설정 (페이지 번호에 따른 회전값 설정)
+    const pageRotationConfig = { // 새로 추가된 객체: 각 페이지에 맞는 회전 각도와 속도 설정
+        0: {
+            angle: 0
+        }, // 첫 페이지 (회전각 0도)
+        1: {
+            angle: 90
+        }, // 두 번째 페이지 (회전각 45)
+        2: {
+            angle: 90
+        }, // 세 번째 페이지 (회전각 90)
+        3: {
+            angle: 135
+        }, // 네 번째 페이지 (회전각 135)
+        4: {
+            angle: 180
+        }, // 다섯 번째 페이지 (회전각 180)
+        5: {
+            angle: 225
+        }, // 여섯 번째 페이지 (회전각 252)
+        6: {
+            angle: 270
+        } // 일곱 번째 페이지 (회전각 270)
+    };
 
+    const pageStyleConfig = {
+        0: {
+            square1: { width: '50vw', height: '100vh', top: '0', right: '0' },
+            square2: { width: '50vw', height: '100vh', top: '0', left: '0' }
+        },
+        1: {
+            square1: { width: '60vw', height: '90vh', top: '10%', right: '0' },
+            square2: { width: '40vw', height: '80vh', top: '5%', left: '0' }
+        },
+        2: {
+            square1: { width: '70vw', height: '80vh', top: '20%', right: '0' },
+            square2: { width: '30vw', height: '70vh', top: '10%', left: '0' }
+        },
+        // 다른 페이지에 대해서도 설정 가능
+        // 예시로 추가한 페이지들
+        3: {
+            square1: { width: '80vw', height: '70vh', top: '30%', right: '0' },
+            square2: { width: '20vw', height: '60vh', top: '15%', left: '0' }
+        },
+        4: {
+            square1: { width: '100vw', height: '60vh', top: '40%', right: '0' },
+            square2: { width: '0', height: '0', top: '50%', left: '0' }
+        },
+        5: {
+            square1: { width: '50vw', height: '50vh', top: '50%', right: '0' },
+            square2: { width: '50vw', height: '50vh', top: '50%', left: '0' }
+        },
+        6: {
+            square1: { width: '100vw', height: '100vh', top: '0', right: '0' },
+            square2: { width: '0', height: '0', top: '0', left: '0' }
+        }
+    };
 
 
     //////////////// 자동스크롤 구현 ////////////////////
@@ -69,8 +90,6 @@ $(function () { ////// jQB //////////////////////
             //////////////////////////////
             //-> 지정시간동안 스크롤막기
             //-> 지정시간은 dur_sc 상수로 관리함!
-
-            // console.log("마우스휠~!");
 
             // e.preventDefault();
             // return false;
@@ -91,38 +110,6 @@ $(function () { ////// jQB //////////////////////
             let delta = e.wheelDelta || e.detail;
             // console.log("휠델타값:" + delta);
 
-            /***************************************** 
-                파이어폭스일때 델타값 부호 반대로 하기
-                _____________________________________
-                방법: JS의 내장함수 test()를 사용하여
-                브라우저 정보를 읽어오는 
-                navigator.userAgent 를 이용!
-                브라우저 정보안에 "Firefox"라는 정보가 있으면
-                text() 에서 true를 리턴함! 파이어폭스인지 구분!
-                [ test() 정규식 메서드 ]
-                정규식.test(값) -> 값 문자열에 정규식문자가
-                 있는지 검사하여 있으면 true리턴
-                 [ 간단한 정규식 표현기호 ]
-                 1. 정규식 내용은 따옴표를 쓰지 않고 슬래쉬 사이에 씀
-                 2. 모든 패턴 문자열을 찾을때는 g라는 플래그문자를 씀
-                 3. 대소문자 구분없이 찾을때는 i라는 플래그문자를 씀
-                 -> flag문자라는 말은 알려주는 표시문자문법임
-                예) /,/g
-                    -> 모든 콤마를 찾아라!
-                    /firefox/i 
-                    -> 대소문자 관계없이 'firefox' 문자를 찾아라!
-                    /my/gi
-                    -> 대소문자 관계없이 'my'문자를 모두 찾아라!
-            *****************************************/
-        //    console.log("브라우저정보:",navigator.userAgent);
-        //    console.log("정보여부:",
-        //    /firefox/i.test(navigator.userAgent));
-
-        // 파이어폭스 브라우저 이면 델타값 부호를 반대로 한다!
-        if(/firefox/i.test(navigator.userAgent)){
-            delta = -delta; // 변수앞에 마이너스는 부호반대!
-        } ////////////////// if /////////////////////
-
             //********************************/
             // 2. 방향에 따른 페이지 번호증감하기 ////
             if (delta < 0) { // 음수면 스크롤 아랫방향(다음페이지)
@@ -134,7 +121,35 @@ $(function () { ////// jQB //////////////////////
                 if (pno === -1) pno = 0; //첫번호에 고정
             } //////// else //////////
 
-            // console.log("페이지번호:", pno);
+
+            // 페이지 번호에 따른 회전 각도 설정
+            rotationAngle = pageRotationConfig[pno].angle; // 페이지에 맞는 회전 각도 설정
+
+            // 각도 변경: model-viewer의 camera-orbit을 설정
+            $("model-viewer").attr("camera-orbit", `${rotationAngle}deg 75deg 100%`);
+
+
+                    // 페이지 번호에 맞춰 square1과 square2의 애니메이션 변경
+        let square1Styles = pageStyleConfig[pno].square1;
+        let square2Styles = pageStyleConfig[pno].square2;
+
+        // square1 애니메이션
+        $(".square1").stop().animate({
+            width: square1Styles.width,
+            height: square1Styles.height,
+            top: square1Styles.top,
+            right: square1Styles.right
+        }, dur_sc, easing_sc);
+
+        // square2 애니메이션
+        $(".square2").stop().animate({
+            width: square2Styles.width,
+            height: square2Styles.height,
+            top: square2Styles.top,
+            left: square2Styles.left
+        }, dur_sc, easing_sc);
+
+
 
             //***************************
             // 3. 페이지번호를 높이값에 곱하여 스크롤 이동하기
@@ -147,24 +162,9 @@ $(function () { ////// jQB //////////////////////
             }, dur_sc, easing_sc);
             // dur_sc 상수에 공통시간설정
 
-            // ****************************************
-            // 4. 클릭시 .gnb+.indic 의 li에 클래스 on넣기
-            $(".gnb li").eq(pno).addClass("on")
-            // $(gnb전체li).eq(해당순번).클래스넣기("on")
-            .siblings().removeClass("on");
-            // .다른형제들().클래스제거("on")
-
-            $(".indic li").eq(pno).addClass("on")
-            // $(indic전체li).eq(해당순번).클래스넣기("on")
-            .siblings().removeClass("on");
-            // .다른형제들().클래스제거("on")
-
-
 
         }); //////////// mousewheel //////////////////////
     ///////////////////////////////////////////////////
-
-
 
 }); ////////////// jQB /////////////////////////
 ////////////////////////////////////////////////
